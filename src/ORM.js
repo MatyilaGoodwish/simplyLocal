72a99490ebf868909d021d62dbe5503e4a2cf832
Goodwish Matyila <goodwish@axxess.co.za>
2019-06-05 16:46:33 +0200

    update firebase update

--- src/ORM.js
+++ src/ORM.js
@@ -5,12 +5,32 @@
  * @country: South Africa
  * CRUD LocalStorage ORM Utils
  */
-
  const bucket = localStorage;
  const CRUD = {
       Save: (bucketName) => bucket.setItem(bucketName, JSON.stringify(eval(bucketName))),
       Delete: (bucketName) => bucket.setItem(bucketName, JSON.stringify(null)),
       Read: (bucketName) => JSON.parse(bucket.getItem(bucketName)),
-      Create: (bucketName) => bucket.setItem(bucketName, null)
+      Create: (bucketName) => bucket.setItem(bucketName, null),
+      Config: (options)=>{
+            firebase: false
+      },
+      //update to firebase
+      Database: () => {
+            return {
+                  Update:  (bucketName) =>{
+                        return firebase.auth().onAuthStateChanged(function(user){
+                              if(user){
+                                    firebase.firestore().collection(bucketName).add(ORM().Read(bucketName))
+                              }else{
+                                    console.warn("configure auth keys for firebase on your project")
+                              }
+                        });
+                  }
+            }
+      }
 }
 const  ORM = () => Object.create(CRUD);
+
+
+
+
