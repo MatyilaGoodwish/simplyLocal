3b09552dec5243356c985a546c90490943dbce5a
Goodwish Matyila <goodwish@axxess.co.za>
2019-06-05 03:07:56 +0200

    initial commit
    ORM Local Storage with example in HTML intermediate use of JavaScript

---
+++ src/ORM.js
@@ -0,0 +1,16 @@
+/**
+ * @author: Goodwish
+ * @copyright: 2019
+ * @license MIT
+ * @country: South Africa
+ * CRUD LocalStorage ORM Utils
+ */
+
+ const  bucket = localStorage;
+ const CRUD =  {
+      Save:  (bucketName) =>   bucket.setItem(bucketName, JSON.stringify(eval(bucketName))),
+      Delete: (bucketName) => bucket.setItem(bucketName, JSON.stringify(null)),
+      Read:  (bucketName) => JSON.parse(bucket.getItem(bucketName)),
+      Create:  (bucketName) =>  bucket.setItem(bucketName, null)
+}
+const  ORM = () => Object.create(CRUD);
